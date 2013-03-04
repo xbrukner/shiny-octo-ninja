05f9776be70d4843592426e54d4d2663370202d6
Tomas Brukner <xbrukner@fi.muni.cz>
2013-03-04 18:19:26 +0100

    Added two more methods of calculating pi.

--- lekce02/pi.py
+++ lekce02/pi.py
@@ -14,6 +14,16 @@ def gregory_leibnitz(limit):
 		divisor += 2
 	return result
 
+def nilakantha(limit):
+	result = 3.0
+	dividend = 1
+	for i in xrange(1, limit + 1):
+		div = 2 * i
+		result += (4.0 / (div * (div + 1) * (div + 2)) ) * dividend
+		dividend *= -1
+		
+	return result
+	
 def archimedes(limit):
 	a = 2.0 * math.sqrt(3)
 	b = 3.0
@@ -21,6 +31,19 @@ def archimedes(limit):
 		a = (2.0 * a * b) / (a + b)
 		b = math.sqrt(a * b)
 	return (a + b) / 2
+
+def gauss_legendre(limit):
+	a = 1.0
+	b = 1.0 / math.sqrt(2)
+	t = 0.25
+	p = 1.0
+	for i in xrange(0, limit):
+		a_1 = (a + b) / 2
+		b = math.sqrt(a*b)
+		t = t - p * ((a - a_1) ** 2)
+		p = 2*p
+		a = a_1
+	return ((a + b) ** 2) / (4 * t)
 
 def montecarlo(limit):
 	good = 0
@@ -34,11 +57,15 @@ def montecarlo(limit):
 	return (4 * good) / float(limit)
 
 def main():
-	limit = 1000000
+	limit = 1000
 	print "Gregory Leibnitz:"
 	print gregory_leibnitz(limit)
+	print "Nilakantha:"
+	print nilakantha(limit)
 	print "Archimedes:"
 	print archimedes(limit)
+	print "Gauss_legedre:"
+	print gauss_legendre(limit)
 	print "Monte carlo:"
 	print montecarlo(limit)
 	print "math.pi"
